dd0d0c80370acbb228d6ad374b07796c3fcc1d3d
imseohyeon <forestlight749@gmail.com>
2025-05-25 21:34:27 +0900

    feat (0522): 테스트 코드

---
+++ java-to-kotlin-my-test/src/test/java/com/grepp/javatokotlinmytest/JavaToKotlinMyTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grepp.javatokotlinmytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaToKotlinMyTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ java-to-kotlin-my-test/src/test/java/com/grepp/javatokotlinmytest/service/UserServiceTest.java
@@ -0,0 +1,67 @@
+package com.grepp.javatokotlinmytest.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import com.grepp.javatokotlinmytest.entity.User;
+import com.grepp.javatokotlinmytest.repository.UserRepository;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    // C
+    @Test
+    void createUser(){
+        //given
+        String name = "jko";
+        String email = "jko@google.com";
+        User user = new User(1L, name, email);
+
+        when(userRepository.save(any(User.class))).thenReturn(user);
+
+        // when
+        User actual = userService.createUser(name, email);
+
+        // then
+        assertNotNull(actual);
+        assertEquals("jko", actual.getName());
+        assertEquals("jko@google.com", actual.getEmail());
+    }
+
+    // R
+    @Test
+    void testGetUser() {
+        // given
+        String name = "jko";
+        String email = "jko@google.com";
+        User user = new User(1L, name, email);
+
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+
+        // when
+        Optional<User> actual = userService.findUser(1L);
+
+        // then
+        assertTrue(actual.isPresent());
+        assertEquals(user, actual.get());
+    }
+}
